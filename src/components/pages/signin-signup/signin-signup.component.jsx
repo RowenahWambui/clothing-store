4cac07808eeefe345f55dadc85cdf210a4dc5c5d
RowenahWambui <suzygracy@gmail.com>
2020-08-22 00:30:48 +0300

    added the signin page and signin component

---
+++ src/components/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../signin/signin.component";
+
+import "./signin-signup.styles.scss";
+
+const SignInSignUpPage = () => (
+  <div className="sign-in-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUpPage;
