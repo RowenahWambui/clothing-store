9d18be1aef8696f99d3f452bbe2856dd78dd27dd
RowenahWambui <suzygracy@gmail.com>
2020-08-21 12:19:44 +0300

    breaking down the homepage component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import HomePage from './components/homepage.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./components/pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div >
+    <div>
       <HomePage />
     </div>
   );
