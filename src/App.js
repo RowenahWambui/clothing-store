5e580b7e8d2b311afc1158b3a212ea109d547be6
RowenahWambui <suzygracy@gmail.com>
2020-08-23 15:31:54 +0300

    added sign-up, implemented firebase utils and authenticated users into firebase db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
 import SignInSignUpPage from "./components/pages/signin-signup/signin-signup.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapShot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
 
-      console.log(user);
+      this.setState({ currentUser: userAuth });
     });
   }
 
